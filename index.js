ae1fbf6c47ce8554dd2b080f84f0ee662c0fba5d
Jaeger <alexbleiz4335@gmail.com>
2024-11-28 09:31:49 -0300

    Servido web

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const express= require ("express");
+const app= express();
+
+
+
+//Rutas
+app.get("/", (req,res)=>{
+    res.send("Hola Mundo!")
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT,()=> console.log(`http://localhost: ${PORT}`));
